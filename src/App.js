dc8b14386fea94d606c86e2aff1a0662b907ad84
unknown <sr.samuelx@gmail.com>
2021-03-16 01:41:23 -0500

    haciendo cambios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>App</code> Mi aplicacion React
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Samuel Parada
         </a>
       </header>
     </div>
